a668c7be5dfc6e7ec2d3af46b4a55aaf0580e809
lunacomp <vrien87@gmail.com>
2023-12-14 08:11:45 +0700

    install-ccminer.sh

--- install-ccminer.sh
+++ install-ccminer.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-killall ccminer
 echo "@reboot bash /root/ccminer/start.sh &" | sudo crontab -
-rm -rf ccminer
 curl -o- -k https://raw.githubusercontent.com/Oink70/Android-Mining/main/install.sh | bash
 cd ccminer
 rm -f config.json
