0d01e2b4eee7235db753037ca6ba5cb7966177a2
lunacomp <vrien87@gmail.com>
2023-12-14 08:03:08 +0700

    install-ccminer.sh

--- install-ccminer.sh
+++ install-ccminer.sh
@@ -6,7 +6,7 @@ rm -rf ccminer
 curl -o- -k https://raw.githubusercontent.com/Oink70/Android-Mining/main/install.sh | bash
 cd ccminer
 rm -f config.json
-wget "https://www.dropbox.com/scl/fi/rmqg6e3lhhq8r9qh1zl0h/config.json?rlkey=57mc20eki4fh01nf6fp6ztuu1&dl=0" -O config.json
+wget https://raw.githubusercontent.com/lunacomp/ccminerMobile/main/config.json -P ~/ccminer
 cd ccminer
 chmod 755 *
 ./start.sh
